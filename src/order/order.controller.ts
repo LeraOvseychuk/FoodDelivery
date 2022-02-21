7b8d80c677c1d550184f75a4f752c5b19ba040fc
LeraOvseychuk <valeriaovsejcuk@MacBook-Pro-Valeria.local>
2022-02-21 21:04:52 +0300

    first commit

---
+++ src/order/order.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+import { Crud, CrudController } from '@nestjsx/crud';
+import { Order } from './order.entity';
+import { OrderService } from './order.service';
+
+@Crud({
+    model: {
+        type: Order
+    }
+    })
+
+@Controller('order')
+export class OrderController implements CrudController<Order> {
+    constructor(public service: OrderService) {}
+}
---
+++ src/order/order.entity.ts
@@ -0,0 +1,26 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { OrderItem } from "src/order-item/order-tem.entity";
+import { User } from "src/user/user.entity";
+import { Column, Entity, JoinColumn, ManyToOne, OneToOne, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Order {
+    @PrimaryGeneratedColumn()
+    orderId: number;
+
+    @ApiProperty()
+    @Column()
+    @ManyToOne(() => User, (customer) => customer.id, { onUpdate: 'CASCADE', onDelete: 'SET NULL' })
+
+    @JoinColumn()
+    customer: number;
+
+    @ApiProperty()
+    @Column({ type: 'simple-json' })
+    @OneToMany(() => OrderItem, (orderItem) => orderItem.order, {
+        cascade: ['update', 'insert'],
+      })
+
+    @JoinColumn()
+     orderItems: OrderItem[]
+  }
